b44b0d5c991b880fc88a14e6f56f76bfbb5564b2
rahmenihamdi2026 <rahmenihamdi2026@gmail.com>
2026-08-31 23:40:09 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Service Worker — Hamdi & Yossr 💍 — v22
+// Service Worker — Hamdi & Yossr 💍 — v23
 
 // ===== FCM Push (notif w l app msakra) =====
 importScripts("https://www.gstatic.com/firebasejs/11.6.0/firebase-app-compat.js");
@@ -28,7 +28,7 @@ self.addEventListener("notificationclick", event => {
   event.waitUntil(clients.openWindow("./index.html"));
 });
 
-const CACHE_NAME = "hamdi-yossr-v22";
+const CACHE_NAME = "hamdi-yossr-v23";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
